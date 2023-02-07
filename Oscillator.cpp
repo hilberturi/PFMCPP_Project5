00c8882ee7a62d0e27f4f22f3a70c8794dfd59ab
hilberturi <122933097+hilberturi@users.noreply.github.com>
2023-02-07 15:20:06 +0000

    Proj. 5, Pt. 4: added required header- and cpp files, moved Oscillator class to Oscillator.h/.cpp

---
+++ Oscillator.cpp
@@ -0,0 +1,235 @@
+#include "Oscillator.h"
+#include <iostream>
+#include <cmath>
+
+static double piTwice = 2 * 3.1415926535897932384626433832795;
+static float piTwiceFloat = static_cast<float>(2 * 3.1415926535897932384626433832795);
+
+
+// Implementation of nested UDT Oscillator::SingleCycleWaveform:
+Oscillator::SingleCycleWaveform::SingleCycleWaveform (int numberOfSupportPoints)
+    : numSupportPoints(numberOfSupportPoints), phaseIncrement(piTwice / numSupportPoints)
+{
+    std::cout << "constructor SingleCycleWaveform" << std::endl; 
+    
+    // the constructor should init the array of equidistant sampled values,
+    // but at these point we do not know about collections yet.
+}
+
+Oscillator::SingleCycleWaveform::~SingleCycleWaveform()
+{
+    std::cout << "destructor SingleCycleWaveform" << std::endl; 
+}
+
+//     1) compute interpolated sample, phase in [0..2 pi)
+float Oscillator::SingleCycleWaveform::computeInterpolatedSample (float phase, float oscAngularVelocity, bool silent) const
+{
+    if (! silent)
+    {
+        std::cout << "oscAngularVelocity [" << oscAngularVelocity << "]  "
+                     "=> should look up downsampled waveform in a mipmap";
+    }    
+    // we will ignore angularVelocity since for the sine wave, there are 
+    // no partials to take care of
+
+    // is it OK to use modern initializer notation for local variables or does it look awkward:
+    
+    float exactLookupPosition {phase / static_cast<float>(phaseIncrement)};
+    
+    int firstLookupIndex = static_cast<int>(exactLookupPosition);
+
+    float secondLookupIndexWeight = exactLookupPosition - static_cast<float>(firstLookupIndex);
+    
+    if (firstLookupIndex == numSupportPoints)
+    {
+        // this should not be the case since nextPhase < piTwice, but perhaps
+        // it can happen in rare cases due to limited floating point precision 
+        firstLookupIndex = 0;
+    }
+    
+    int secondLookupIndex = firstLookupIndex + 1;
+    
+    if (secondLookupIndex == numSupportPoints)
+    {
+        secondLookupIndex = 0;    
+    }
+
+    if (interpolationType == 0)
+    {
+        // linear interpolation
+        return (1.0f - secondLookupIndexWeight) * getSampleAtIndex (firstLookupIndex) 
+               + secondLookupIndexWeight * getSampleAtIndex (secondLookupIndex);
+    }
+
+    // else: no interpolation at all
+    return getSampleAtIndex (firstLookupIndex);
+}
+
+// helper function getSampleAtIndex()
+// (should rather be private)
+
+
+//     2) transform shape in various ways
+void Oscillator::SingleCycleWaveform::transformShape (bool doFlipPolarity, bool normalize, 
+                                                      bool removeDcOffset, float warp)
+{
+     // for the moment, we only set the flipPolarity and reversePhase flags
+    flipPolarity = doFlipPolarity;
+    std::cout << "transform shape: normalize [" << normalize << "], "
+                 "removeDcOffset [" << removeDcOffset << "], "
+                 "warp [" << warp << "] have no effect yet";
+}
+
+//     3) dump waveform by generating samples at equidistant steps 
+// (as given by numSupportPoints) and showing them on the console.
+void Oscillator::SingleCycleWaveform
+    ::dumpWaveform (float scaling) const
+{
+    
+    for (int i = 0; i < numSupportPoints; ++i)
+    {
+        float sample = scaling * getSampleAtIndex (i);
+        float phase = static_cast<float>(i * phaseIncrement);
+        
+        std::cout << "supportPoint [" << i << "], "
+                     "phase [" << phase << "] "
+                     "=> sample [" << sample << "]"
+                  << std::endl;
+    }
+}
+
+float Oscillator::SingleCycleWaveform::getSampleAtIndex (int sampleIndex) const
+{
+    // This would normally look up the i-th precomputed value
+    // in an array, but we do not know yet about arrays and
+    // similar structures in C++.
+    // no range check
+    float rawSampleAtIndex = static_cast<float>(sin (sampleIndex * phaseIncrement));
+    return (flipPolarity ? -rawSampleAtIndex : rawSampleAtIndex);
+}
+
+void Oscillator::SingleCycleWaveform::printName (const std::string& prefix) const
+{
+    std::cout << prefix << "name: " << this->name << std::endl;
+}
+
+void Oscillator::SingleCycleWaveform::printInterpolationType (const std::string& prefix) const
+{
+    std::cout << prefix << "interpolationType: " << this->interpolationType << std::endl;
+}
+
+void Oscillator::SingleCycleWaveform::printNumSupportPoints (const std::string& prefix) const
+{
+    std::cout << prefix << "numSupportPoints: " << this->numSupportPoints << std::endl;
+}
+
+void Oscillator::SingleCycleWaveform::printPhaseIncrement (const std::string& prefix) const
+{
+    std::cout << prefix << "phaseIncrement: " << this->phaseIncrement << std::endl;
+}
+
+void Oscillator::SingleCycleWaveform::printFlipPolarity (const std::string& prefix) const
+{
+    std::cout << prefix << "flipPolarity: " << this->flipPolarity << std::endl;
+}
+
+void Oscillator::SingleCycleWaveform::printWaveform (const std::string& prefix) const
+{
+    this->printName (prefix);
+    this->printInterpolationType (prefix);
+    this->printNumSupportPoints (prefix);
+    this->printPhaseIncrement (prefix);
+    this->printFlipPolarity (prefix);
+}
+
+// Implementation of struct Oscillator
+
+Oscillator::Oscillator (std::string id, float rate)
+    : oscillatorId(id), sampleRate(rate)
+{
+    std::cout << "constructor Oscillator" << std::endl;
+}
+
+Oscillator::~Oscillator()
+{
+    std::cout << "destructor Oscillator" << std::endl; 
+}
+
+//     1) reset to given frequency and initial phase
+void Oscillator::reset (float frequency, float initialPhase)
+{
+    angularVelocity = static_cast<float>(piTwice) * frequency / sampleRate;
+    currentPhase = initialPhase;
+}
+
+//     2) generate sample
+float Oscillator::generateSample()
+{
+    float sample = waveform.computeInterpolatedSample(currentPhase, angularVelocity);
+
+    // prepare for next sample
+    currentPhase += angularVelocity;
+    
+    if (currentPhase >= piTwiceFloat)
+    {
+        currentPhase -= piTwiceFloat;
+    }
+    
+    return sample;
+}
+
+ //     3) dump a given number of samples to console Samples (optionally without changing internal state)
+void Oscillator::dumpSamples (int numSamples, bool restoreCurrentPhase)
+{
+    float currentPhaseBeforeDumpSamples = currentPhase;
+    
+    for (int i = 0; i < numSamples; ++i)
+    {
+        float savedPhase = currentPhase;
+        float sample = generateSample();
+        
+        std::cout << "index [" << i << "], "
+                     "phase [" << savedPhase << "] "
+                     "=> sample [" << sample << "]"
+                  << std::endl;
+    }
+
+    if (restoreCurrentPhase)
+    {
+        currentPhase = currentPhaseBeforeDumpSamples;
+    }
+}
+
+void Oscillator::printOscillatorId (const std::string& prefix) const
+{
+    std::cout << prefix << "oscillatorId: " << this->oscillatorId << std::endl;
+}
+
+void Oscillator::printSampleRate (const std::string& prefix) const
+{
+    std::cout << prefix << "sampleRate: " << this->sampleRate << std::endl;
+}
+
+void Oscillator::printAngularVelocity (const std::string& prefix) const
+{
+    std::cout << prefix << "angularVelocity: " << this->angularVelocity << std::endl;
+}
+
+void Oscillator::printCurrentPhase (const std::string& prefix) const
+{
+    std::cout << prefix << "currentPhase: " << this->currentPhase << std::endl;
+}
+
+void Oscillator::printWaveform (const std::string& prefix) const
+{
+    this->waveform.printWaveform (prefix + "waveform.");
+}
+
+void Oscillator::printOscillator (const std::string& prefix) const
+{
+    this->printOscillatorId (prefix);
+    this->printSampleRate (prefix);
+    this->printAngularVelocity (prefix);
+    this->printCurrentPhase (prefix);
+    this->printWaveform (prefix);
+}
---
+++ Oscillator.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "LeakedObjectDetector.h"
+#include <string>
+
+struct Oscillator
+{
+    struct SingleCycleWaveform 
+    {    
+        SingleCycleWaveform (int numSupportPoints);
+        ~SingleCycleWaveform();
+
+        // 5 properties:
+        //    1) name (std::string)
+        std::string name {"sine"};
+        //    2) interpolation type (int), 0 means linear interpolation 
+        int interpolationType {0};
+        //    3) number of equidistant support points with sampled data (int)
+        int numSupportPoints;
+        //    4) phase increment between adjacent support points 
+        double phaseIncrement;
+        //    5) flip polarity flag which controls polarity reversal (bool)
+        bool flipPolarity {false};
+
+        // 3 things it can do:
+        //     1) compute interpolated sample given phase and angular velocity
+        float computeInterpolatedSample (float phase, float oscAngularVelocity, bool silent = true) const;
+
+        //     2) transform shape in various ways
+        void transformShape (bool flipPolarity = false, bool normalize = false, 
+                             bool removeDcOffset = false, float warp = 0);
+
+        //     3) dump waveform by generating samples at equidistant steps (as
+        //        given by numSupportPoints) and showing them on the console.
+        void dumpWaveform (float scaling = 1) const;
+
+        // I need an internal helper function, 0 <= sampleIndex < numSupportPoints
+        float getSampleAtIndex (int sampleIndex) const;
+
+        // print functions for members, Part 2
+        void printName (const std::string& prefix = "this->") const;
+        void printInterpolationType (const std::string& prefix = "this->") const;
+        void printNumSupportPoints (const std::string& prefix = "this->") const;
+        void printPhaseIncrement (const std::string& prefix = "this->") const;
+        void printFlipPolarity (const std::string& prefix = "this->") const;
+        void printWaveform (const std::string& prefix = "this->") const;
+    };
+
+    Oscillator (std::string oscillatorId, float sampleRate = 44100);
+    ~Oscillator();
+
+    // 5 properties:
+    //     1) oscillator identifier (std::string)
+    std::string oscillatorId;
+    //     2) waveform (SingleCycleWaveform)
+    SingleCycleWaveform waveform {16};
+    //     3) sample rate (float)
+    float sampleRate;
+    //     4) angular velocity
+    float angularVelocity;
+    //     5) current phase (float)
+    float currentPhase {0};
+
+    // 3 things it can do:
+    //     1) reset to given frequency and initial phase
+    void reset (float frequency, float initialPhase = 0);
+
+    //     2) generate sample
+    float generateSample(); // returns generated sample
+
+    //     3) dump a given number of samples to console Samples (optionally without changing internal state)
+    void dumpSamples (int numSamples, bool restoreCurrentPhase = false);
+
+    // print functions for members, Part 2
+    void printOscillatorId (const std::string& prefix = "this->") const;
+    void printSampleRate (const std::string& prefix = "this->") const;
+    void printAngularVelocity (const std::string& prefix = "this->") const;
+    void printCurrentPhase (const std::string& prefix = "this->") const;
+    void printWaveform (const std::string& prefix = "this->") const;
+    void printOscillator (const std::string& prefix = "this->") const;
+
+    JUCE_LEAK_DETECTOR(Oscillator)
+};
